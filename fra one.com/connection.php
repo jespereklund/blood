77e0bdac9e297f02ea0aed5d88a224ce16a8ed39
Jesper Eklund <jespereklund@gmal.com>
2026-03-12 13:26:10 +0100

    slettet

--- fra one.com/connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$servername = "localhost";
-    $username = "flettedehvaler_";
-    $password = "pfTgy5GP";
-    $database = "flettedehvaler_";
-
-    $conn = new mysqli($servername, $username, $password, $database);
-?>
